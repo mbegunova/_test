2c8a4cb3b927729bd9b6ea588ccad729818baddd
m.begunova <begunova.masha00@mail.ru>
2021-09-15 23:33:17 +0300

    branch f-1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
   
     public static void main(String[] args) {
           System.out.print("A");
-        System.out.print("Hello world!");
+        System.out.print("Hello world!1");
 	System.out.print("B");
     }
 }
