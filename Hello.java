6704d4d41fa1ddb6eec4f0c129122321f128a3d6
m.begunova <begunova.masha00@mail.ru>
2021-09-15 23:03:49 +0300

    hello world

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+  
+    public static void main(String[] args) {
+          
+        System.out.print("Hello world!2");
+
+	System.out.print("B");
+	System.out.print("A");
+    }
+}
