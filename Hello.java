c29b98a49fb292f2282a49aa6b3399fd3edd7405
m.begunova <begunova.masha00@mail.ru>
2021-09-15 23:23:57 +0300

    added B

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello{
   
     public static void main(String[] args) {
-          
+          System.out.print("A");
         System.out.print("Hello world!");
 		
-	System.out.print("A");
+	
     }
 }
